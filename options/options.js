7de9f0f6771dca7e85097a14a93295696ce7b28c
Crowdbotics <team@crowdbotics.com>
2021-08-07 16:13:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aged_dust_29614Identifier",
+  url: "https://aged_dust_29614Identifier.botics.co",
+  api: "https://aged_dust_29614Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
